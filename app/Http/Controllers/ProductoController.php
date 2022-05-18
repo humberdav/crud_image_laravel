fcb18eee254ec4ce6560373da20561997cb8bef3
Humberto Valdez <humbertodavid.valdez@gmail.com>
2022-05-18 16:32:49 -0500

    Commit inicial del proyecto

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $productos = Producto::all();
+        return view('productos.index')->with('productos',$productos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('productos.crear');
+        // console.log('hola');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required', 'descripcion' => 'required', 'imagen' => 'required|image|mimes:jpeg,png,svg|max:1024'
+        ]);
+        $producto = $request->all();
+        if($imagen = $request->file('imagen')){
+            $rutaGuardar = 'imagen/';
+            $imagenProducto = date('YmdHis').".".$imagen->getClientOriginalExtension();
+            $imagen->move($rutaGuardar,$imagenProducto);
+            $producto['imagen'] = $imagenProducto;
+        }
+        Producto::create($producto);
+        return redirect()->route('productos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Producto $producto)
+    {
+        return view('productos.editar',compact('producto'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Producto $producto)
+    {
+        $request->validate([
+            'nombre' => 'required', 'descripcion' => 'required'
+        ]);
+         $prod = $request->all();
+         if($imagen = $request->file('imagen')){
+            $rutaGuardarImg = 'imagen/';
+            $imagenProducto = date('YmdHis') . "." . $imagen->getClientOriginalExtension(); 
+            $imagen->move($rutaGuardarImg, $imagenProducto);
+            $prod['imagen'] = "$imagenProducto";
+         }else{
+            unset($prod['imagen']);
+         }
+         $producto->update($prod);
+         return redirect()->route('productos.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Producto $producto)
+    {
+        $producto->delete();
+        return redirect()->route('productos.index');
+    }
+}
---
+++ resources/views/productos/index.blade.php
@@ -0,0 +1,77 @@
+<x-app-layout>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ __('Productos') }}
+      </h2>
+  </x-slot>
+
+  <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <a href="{{ route('productos.create') }}" type="button" class="bg-indigo-500 px-12 py-2 rounded text-gray-200 font-semibold hover:bg-indigo-800 transition duration-200 each-in-out">Crear</a>
+              <table class="table-fixed w-full">
+                <thead class="bg-gray-800 text-white">
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th>Imagen</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($productos as $producto)
+                    <tr>
+                        <td>{{ $producto->id }}</td>
+                        <td>{{ $producto->nombre }}</td>
+                        <td>{{ $producto->descripcion }}</td>
+                        <td>
+                            <img src="/imagen/{{ $producto->imagen }}" width="20%" >
+                        </td>
+                        <td>
+                            <div class="flex justify-center rounded-gl text-lg" role="group">
+                                <a href="{{route('productos.edit', $producto->id)}}" class="rounded bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4">Editar</a>
+                                <form action="{{ route('productos.destroy', $producto->id) }}" method="POST" class="formEliminar">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="rounded bg-pink-400 hover:bg-pink-500 text-white font-bold py-2 px-4">Eliminar</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>    
+                    @endforeach
+                    
+                </tbody>
+              </table>
+          </div>
+      </div>
+  </div>
+</x-app-layout>
+<script>
+    (function () {
+        'use strict'
+        //debemos crear la clase formEliminar dentro del form del boton borrar
+        //recordar que cada registro a eliminar esta contenido en un form  
+        var forms = document.querySelectorAll('.formEliminar')
+        Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+        form.addEventListener('submit', function (event) {        
+            event.preventDefault()
+            event.stopPropagation()        
+            Swal.fire({
+                    title: '¿Confirma la eliminación del registro?',        
+                    icon: 'question',
+                    showCancelButton: true,
+                    confirmButtonColor: '#20c997',
+                    cancelButtonColor: '#6c757d',
+                    confirmButtonText: 'Confirmar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        this.submit();
+                        Swal.fire('¡Eliminado!', 'El registro ha sido eliminado exitosamente.','success');
+                    }
+                })                      
+        }, false)
+        })
+    })()
+  </script>
